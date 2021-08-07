ddbbef5442648b79b8bc6decabe62e6f9010fc80
cedric <none@none.none>
2021-08-07 17:35:09 +0200

    Use more up to date js

--- static/getScanResult.js
+++ static/getScanResult.js
@@ -1,76 +1,49 @@
-function __deleteChild(parentId)
+function deleteChild(parent)
 {
-	let parent = document.getElementById(parentId);
-
 	while (parent.lastElementChild) {
 		parent.removeChild(parent.lastElementChild);
 	  }
 }
 
-function __showDataInDom(parentId, 
-						 short, 
-						 episode_number, 
-						 older_episode_number,
-						 siteUrl)
+function insertRow(parent, short, episode_number, older_episode_number, endpoint)
 {
+	const row = parent.insertRow();
+	const cell = [];
+	for(let i=0; i < 4; ++i)
+	{
+		cell.push(row.insertCell());
 
-	let parent = document.getElementById(parentId);
+	}
 
-	line                 = document.createElement("tr");
-	columnName           = document.createElement("td");
-	columnCurrentEpisode = document.createElement("td");
-	columnEpisodeBefore  = document.createElement("td");
-	columnUrl  			 = document.createElement("td");
-	url                  = document.createElement("a");
-
-	columnName.textContent             = short;
-	columnCurrentEpisode.textContent   = episode_number; 
-	columnEpisodeBefore.textContent    = older_episode_number;
+	cell[0].innerHTML = short; 
+	cell[1].innerHTML = episode_number; 
+	cell[2].innerHTML = older_episode_number;
+	cell[3].innerHTML = `<a target="__blank" href="${endpoint}">${endpoint}</a>`
 	
-	columnUrl.append(url);
-	url.setAttribute("href", siteUrl);
-	url.setAttribute("target", "__blank");
-	url.textContent = siteUrl;
-
-	line.append(columnName);
-	line.append(columnCurrentEpisode);
-	line.append(columnEpisodeBefore);
-	line.append(columnUrl);
-
-	parent.append(line)
-	
-	
-		
 }
 
-function getScanResult(target)
+
+function scan(e)
 {
-	let request = new XMLHttpRequest();
 	
-	request.open("GET", "/getData", true);
-	request.setRequestHeader('Content-type', 'application/json');
-	
-	request.onreadystatechange = function() {
+	fetch("/getData")
+	.then(blob   => blob.json())
+	.then(series => {
 		
-		if (request.readyState === XMLHttpRequest.DONE && 
-		    request.status     ===  200) {
-		
-			let answer = JSON.parse(request.responseText);
-			__deleteChild(target)
-			
-			if (!answer) return;
+		const displayTarget = document.getElementById(e.target.dataset.target);
+		deleteChild(displayTarget);
 
-			for (let serie of answer.serie ) {
-
-				__showDataInDom(target, 
-					            serie.short, 
-								serie.episode_number, 
-								serie.older_episode_number,
-								serie.url);
-			}
+		for(serie of series.series)
+		{
+			insertRow(displayTarget,serie.short, 
+					  serie.episode_number, 
+					  serie.older_episode_number,
+					  serie.url);
 		}
-	};
 
-	request.send();
+	});
 
 }
+
+scanButton = document.querySelector(".scan-button");
+scanButton.addEventListener("click", scan);
